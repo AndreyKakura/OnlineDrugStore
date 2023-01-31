a74c326380d6997e97243bea41f1eeb618cf6083
Andrey <andreykakura@gmail.com>
2023-01-31 14:46:31 +0300

    feat: Added BadCredentialsException handling

--- src/main/java/by/bsuir/drugstore/exception/CustomExceptionHandler.java
+++ src/main/java/by/bsuir/drugstore/exception/CustomExceptionHandler.java
@@ -3,6 +3,7 @@ package by.bsuir.drugstore.exception;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,5 +16,12 @@ public class CustomExceptionHandler {
         log.error(e.getMessage());
         ErrorResponse error = new ErrorResponse(e.getLocalizedMessage());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler()
+    public ResponseEntity<ErrorResponse> handleException(BadCredentialsException e) {
+        log.error(e.getMessage());
+        ErrorResponse error = new ErrorResponse(e.getLocalizedMessage());
+        return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
     }
 }
