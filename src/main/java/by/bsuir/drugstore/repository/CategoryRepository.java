55dc5b798ef2381274a34eb23eeab9cbc238920c
ILYA <roshhin-78@mail.ru>
2023-02-03 11:27:12 +0300

    add repository

---
+++ src/main/java/by/bsuir/drugstore/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package by.bsuir.drugstore.repository;
+
+import by.bsuir.drugstore.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
